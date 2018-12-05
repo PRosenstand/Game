b2d8cf8ddc418e42842dd128b4bc8195b411d449
turulix <tigga01@online.de>
2018-12-05 22:18:59 +0100

    I forgot to commit... So here you go.

--- src/Main/GameObjects/Item.java
+++ src/Main/GameObjects/Item.java
@@ -3,6 +3,47 @@ package Main.GameObjects;
 import Main.Room;
 
 public class Item {
-    public Item(Room room) {
+    Room room;
+    String name;
+    String description;
+    int id;
+
+    public Item(Room room, int id, String name, String description) {
+        this.room = room;
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
