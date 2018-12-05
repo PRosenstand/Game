2eda3ae896bf07067a2c79b035727f9f1c56079c
turulix <tigga01@online.de>
2018-12-05 18:56:00 +0100

    Fixed

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -15,6 +15,7 @@ public class GameLauncher {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Im running now okey...");
     }
     
 }
