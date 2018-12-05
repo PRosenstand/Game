70de29b7df3588a731284cf1d678b02f79b036aa
turulix <tigga01@online.de>
2018-12-05 20:00:19 +0100

    sometimes you just herp the derp so hard it herpderps

---
+++ src/UserInterface/Main.java
@@ -0,0 +1,20 @@
+package UserInterface;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primalStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        primalStage.setTitle("Hello World");
+        primalStage.setScene(new Scene(root, 300, 275));
+        primalStage.show();
+    }
+}
