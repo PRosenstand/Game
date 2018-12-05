589124ed27defc4ca39b4d063cb7d512458bcf1d
PRosenstand <38947568+PRosenstand@users.noreply.github.com>
2018-12-05 21:04:51 +0100

    Updated Start description

--- src/Game.java
+++ src/Game.java
@@ -68,8 +68,8 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Welcome to TempGameTitle!");
+        System.out.println("You find yourself in the entrance to a dungeon like cave with a dark forest behind you");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
